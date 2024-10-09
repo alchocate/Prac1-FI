7699036fd3be803c4bc50ef9dcf198d7490e80c2
Your Nme <ericcasanova.25@gmail.com>
2024-10-09 20:57:36 +0200

    Exercisi 2 ara ben fet

--- Practica.sh
+++ Practica.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
 awk -F',' '{ if (NF == 16) print $0 }' CAvideos.csv > supervivents.csv
 cut -d ',' --complement -f12,16 supervivents.csv > sortida.csv
 awk -F',' '$14 != "True"' sortida.csv > nou.csv
+
+x=$(wc -l < sortida.csv)
+
+y=$(wc -l < nou.csv)
+
+
+resul=$((x - y)) #fa la diferencia entre el arxius abans i despres de filtrar
+
+
+echo "Entrades eliminades: $resul" #ho imprimeix
+
+
+
+# awk -F ',' | {if -f 8 <1000000} > -f 17 =="Bo" nou.csv
+
+
+
+
