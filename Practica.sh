2971e26b6e4da42f4fc38a8b532fae4b4f01d0c7
alanfarre <1744371@autonoma.cat>
2024-10-13 21:13:30 +0200

    Ej 4

--- Practica.sh
+++ Practica.sh
@@ -1,37 +1,61 @@
 #!/bin/bash
 
-# Filtra líneas con exactamente 16 campos
+# Filtra les línies que tenen exactament 16 columnes
 awk -F',' '{ if (NF == 16) print $0 }' CAvideos.csv > supervivents.csv
 
-# Elimina las columnas 12 y 16
+# Mostra totes les columnes menys la 12 i la 16
 cut -d ',' --complement -f12,16 supervivents.csv > sortida.csv
 
-# Filtra líneas donde la columna 14 no sea "True"
+# Mostra totes les línies on la columna 14 no sigui "True"
 awk -F',' '$14 != "True"' sortida.csv > nou.csv
 
-# Cuenta las líneas antes y después del filtrado
+# Compta les línies abans de ser filtrades
 x=$(wc -l < sortida.csv)
 y=$(wc -l < nou.csv)
 resul=$((x - y))
 
-# Imprime el número de entradas eliminadas
+# Imprimeix el resultat dels vídeos eliminats
 echo "Entrades eliminades: $resul"
 
-# Añade una columna "Ranking_Views" y clasifica según la columna 8
+# Afegeix una columna "Ranking_Views" i les clasifica segons les visualitzacions (columna 8)
 awk -F',' '
-    NR==1 {
-        print $0",Ranking_Views"
-        next
-    }
-    {
+    	NR==1 {print $0",Ranking_Views"
+        	next
+    	}
+    	{
         if ($8 < 1000000) {
-            print $0",Bo"
+            	print $0",Bo"
         } else if ($8 > 1000000 && $8 <= 10000000) {
-            print $0",Excel·lent"
+            	print $0",Excel·lent"
         } else if ($8 > 10000000) {
-            print $0",Estrella"
+            	print $0",Estrella"
         }
     }
 ' OFS=',' nou.csv > views.csv
 
 
+awk -F',' '
+    NR==1 {print $0", Rlikes, Rdislikes"
+		next
+	}
+{
+Rlikes=0;
+Rdislikes=0;
+for (i = 1; i <= NF; i++){
+		if(i == 8){
+			views =$i;
+		} else if (i == 9){
+			likes =$i
+		} else if (i == 10){
+			dislikes=$i
+		}
+if (views != 0){
+	Rlikes = (100/views)*likes;
+	Rdislikes = (100/views)*dislikes;
+} else{
+	Rlikes = "/"
+	Rdislikes = "/"
+}
+}
+
+}' OFS=',' views.csv > ranking.csv
