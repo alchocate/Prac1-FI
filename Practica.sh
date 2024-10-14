66f2f0b2ae0f7e45b9c126091765f5c86da2371c
Your Nme <ericcasanova.25@gmail.com>
2024-10-14 13:22:52 +0200

    Exercisi 4 completat

--- Practica.sh
+++ Practica.sh
@@ -19,43 +19,62 @@ echo "Entrades eliminades: $resul"
 
 # Afegeix una columna "Ranking_Views" i les clasifica segons les visualitzacions (columna 8)
 awk -F',' '
-    	NR==1 {print $0",Ranking_Views"
-        	next
-    	}
-    	{
+        NR==1 {print $0",Ranking_Views"
+                next
+        }
+        {
         if ($8 < 1000000) {
-            	print $0",Bo"
-        } else if ($8 > 1000000 && $8 <= 10000000) {
-            	print $0",Excel·lent"
+                print $0",Bo"
         } else if ($8 > 10000000) {
-            	print $0",Estrella"
+                print $0 ",Estrella"
+        } else {
+                print $0 ",Exel·lent"
         }
     }
-' OFS=',' nou.csv > views.csv
+' nou.csv > views.csv
 
 
-awk -F',' '
-    NR==1 {print $0", Rlikes, Rdislikes"
-		next
-	}
-{
-Rlikes=0;
-Rdislikes=0;
-for (i = 1; i <= NF; i++){
-		if(i == 8){
-			views =$i;
-		} else if (i == 9){
-			likes =$i
-		} else if (i == 10){
-			dislikes=$i
-		}
-if (views != 0){
-	Rlikes = (100/views)*likes;
-	Rdislikes = (100/views)*dislikes;
-} else{
-	Rlikes = "/"
-	Rdislikes = "/"
-}
-}
 
-}' OFS=',' views.csv > ranking.csv
+
+
+#awk -F',' '
+#    NR==1 {print $0", Rlikes, Rdislikes"
+#		next
+#	}
+#{
+#Rlikes=0;
+#Rdislikes=0;
+#for (i = 1; i <= NF; i++){
+#		if(i == 8){
+#			views =$i;
+#		} else if (i == 9){
+#			likes =$i
+#		} else if (i == 10){
+#			dislikes=$i
+#		}
+#if (views != 0){
+#	Rlikes = (100/views)*likes;
+#	Rdislikes = (100/views)*dislikes;
+#} else{
+#	Rlikes = "/"
+#	Rdislikes = "/"
+#}
+#}
+
+#}' OFS=',' views.csv > ranking.csv
+
+
+
+#
+l1=true
+while IFS=, read -r c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 c11 c12 c13 c14 c15
+do
+  if $l1; then
+	echo $c1","$c2","$c3","$c4","$c5","$c6","$c7","$c8","$c9","$c10","$c11","$c12","$c13","$c14","$c15",Rlikes,Rdislikes"
+	l1=false
+	continue
+  fi
+   echo $c1","$c2","$c3","$c4","$c5","$c6","$c7","$c8","$c9","$c10","$c11","$c12","$c13","$c14","$c15",$((c9 * 100 / c8)),$((c10 * 100 / c8))"
+done < views.csv > ranking.csv
+
+head ranking.csv
